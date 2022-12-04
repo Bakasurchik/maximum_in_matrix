a0da1537594ca997f3be1b9291de43948053b874
Kirill <bakasurchik@gmail.com>
2022-12-04 14:22:02 +0500

    added maximum of matrix function

--- index.py
+++ index.py
@@ -1,10 +1,17 @@
 def filling_matrix(row):
     return [[int(elem) for elem in input().split()] for i in range(row)]
 
+def maximum_in_matrix(matrix):
+    largest = max([max(row) for row in matrix])
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == largest:
+                return [i, j]
+
 def main():
     n = int(input())
     m = int(input())
     matrix = filling_matrix(n)
-    print(*matrix, sep='\n')
+    print(*maximum_in_matrix(matrix))
 
 main()
